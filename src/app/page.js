df9fccc8be5af6eeb4496a6e5c69ff7ddbd08144
zx762 <112207314@g.nccu.edu.tw>
2025-06-07 14:46:54 +0800

    0607: 14:46

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,33 @@
+"use client"
+
+import StartPage from "@/component/StartPage";
+import MusicPreviewPage from "@/component/MusicPreviewPage";
+import HowPage from "@/component/HowPage";
+import GamePage from "@/component/GamePage";
+import ResultPage from "@/component/ResultPage";
+import { useState } from "react";
+import {useStore} from "@/app/store/store";
+import FailedPage from "@/component/FailedPage";
 
 export default function Home() {
+
+  const gameState= useStore( (state)=>state);
+
   return (
     <div>
-      HOME
+      <> 
+        <div className="w-screen h-screen bg-gray-100 flex items-center justify-center">
+            { gameState.state == 0 && <StartPage/>}
+            { gameState.state == 0.5 && <MusicPreviewPage />}
+            { gameState.state == 0.75 && <HowPage />}
+            { gameState.state == 1 && <GamePage/>}
+            { gameState.state == 2 && <ResultPage />}
+            { gameState.state == 3 && <FailedPage />}
+
+        </div>
+        
+        
+      </>
     </div>
   );
 }
